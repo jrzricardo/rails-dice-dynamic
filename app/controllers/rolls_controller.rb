930ae721a34f336b1e10f799f148c9d27aba7d97
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-07 15:50:39 +0000

    set up dice rolls

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,6 +1,22 @@
 class RollsController < ApplicationController
   def home
-    @hi = "howdy"
+    
     render({ :template => "rolls_templates/home"})
+  end
+
+  def dice
+    @num_dice = params.fetch("number_of_dice").to_i
+  
+    @sides = params.fetch("how_many_sides").to_i
+    
+    @rolls = []
+     
+    @num_dice.times do
+      die = rand(1..@sides)
+  
+      @rolls.push(die)
+
+    end
+    render({ :template => "rolls_templates/dice"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "rolls", :action => "home"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "rolls", :action => "dice"})
 end
